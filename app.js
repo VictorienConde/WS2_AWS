a39da38680c7b8eb9abf9b5cd64884d0dbf61ccb
CONDE Victorien <victorien.conde@epitech.eu>
2025-12-10 18:53:34 +0100

    {Fix} change text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello depuis ecr-nodejs-app 🚀 (Docker + AWS ECR)");
+  res.send("Hello depuis ecr-nodejs-app 🚀 (Docker + AWS ECR) xD");
 });
 
 app.listen(PORT, () => {
